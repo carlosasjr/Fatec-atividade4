7e3088900131fac4acbe78c853f7ad904945eae4
bmota2309 <47526394+bmota2309@users.noreply.github.com>
2019-03-25 13:54:14 -0300

    acerto do campo nome pra não permitir envio do nome sem sobrenome [script.js], correção do for no textarea que está incorreto[bruno.html].

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -4,42 +4,53 @@ function validar(){
     var erro = false;
     //Valida Nome
 if (nome.length <=9) {
-$(document).ready(function(){
-    $.notify({
-        message:"ERRO! Campo Nome deve ter no mínimo 10 caracteres"
+    $(document).ready(function(){
+        $.notify({
+         message:"ERRO! Campo Nome deve ter no mínimo 10 caracteres"
     },{
-    type: 'danger',
-    timer: 2000
-    
+        type: 'danger',
+        timer: 2000
+});
 });
 
-});
 erro =true;
 }
+
+//Verifica espaço após nome
+if (nome.trim().indexOf(" ") === -1) {
+    $.notify({
+        message:"Campo nome deve ter um sobrenome!"
+    },{
+            type:('danger'),
+            timer:2000
+});
+    erro = true;
+}
+
+//Valida e-mail
 var sEmail	= $("#email").val();
 		var emailFilter=/^.+@.+\..{2,}$/;
 		var illegalChars= /[\(\)\<\>\,\;\:\\\/\"\[\]]/;		
 		if(!(emailFilter.test(sEmail))||sEmail.match(illegalChars)){
 			$(document).ready(function(){
-    $.notify({
-        message:"E-mail inválido!"
+        $.notify({
+            message:"E-mail inválido!"
     },{
-    type: 'danger',
-    timer: 2000
-    
+        type: 'danger',
+        timer: 2000
 });
 });
-
 erro =true;
-		}	
+}	
+
+//Valida dúvidas
 if (duvidas.length > 400 ) {
 $(document).ready(function(){
     $.notify({
         message:"Campo duvidas excede 400 caracteres"
     },{
-    type: 'danger',
-    timer: 2000
-    
+        type: 'danger',
+        timer: 2000
 });
 });
 erro =true;
@@ -49,17 +60,17 @@ $(document).ready(function(){
     $.notify({
         message:"Campo duvidas está em branco"
     },{
-    type: 'danger',
-    timer: 2000
+        type: 'danger',
+        timer: 2000
     
 });
 });
 erro =true;
 }
-if (erro == false) {
-                                $.notify("Cadastrado com sucesso!", "success");
-                            
+
+//Envia fomulário e gera msg
+if (erro === false) {
+    $.notify("Dúvida enviada com sucesso!", "success");
 }
 return false;
 }
-
